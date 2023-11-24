8031c2dd3b155ad3cb35dea9e197f4ad38531aaf
Jefferson Frota <jeffersonfrotacampos@gmail.com>
2023-11-24 18:23:22 -0300

    primeiro commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,67 @@
+package com.example;
+import java.sql.*;
+import com.example.util.Viewer;
+import java.util.ArrayList;
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            
+            Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/teste", "postgres", "nk29wp38s9");
+            
+            if(conexao != null){
+                System.out.println("Banco conectado! ");
+                Statement stn = conexao.createStatement();
+
+                consultaDados(stn);
+                
+                stn.close();
+                conexao.close();
+            }
+            else{
+                System.out.println("Deu ruim, falhou !");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+    }
+
+    static void consultaDados(Statement stn){
+        String sql = "SELECT * FROM viewer";
+        
+        try {
+           ResultSet result = stn.executeQuery(sql);
+           ArrayList<Viewer> arrayList = new ArrayList<>();
+
+           while(result.next()) {
+                
+                Viewer viewer = null;
+
+                viewer = new Viewer( result.getLong("cpf_cnpj"), result.getString("nome"),  result.getString("email"),  result.getInt("num_seguidores"), result.getString("nacionalidade"), result.getString("status"), result.getString("descricao"));
+
+
+                //System.out.print(viewer.toString());
+
+                arrayList.add(viewer);
+
+                //System.out.println("cpf_cnpj: " + result.getLong("cpf_cnpj") + ", nome: " + result.getString("nome") + ", email: " + result.getString("email") + ", nacionalidade: " + result.getString("nacionalidade") + ", num_seguidores: " + result.getInt("num_seguidores") + ", status: " + result.getString("status")  + ", descricão: " + result.getString("descricao"));    
+           } 
+
+           for(Viewer v : arrayList){
+                System.out.print(v.toString());
+           }
+           
+           result.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+
+    } 
+}
---
+++ src/main/java/com/example/util/Viewer.java
@@ -0,0 +1,81 @@
+package com.example.util;
+
+public class Viewer {
+    private long cpf_cnpj;
+    private String nome, email, nacionalidade, status, descricao;
+    private int num_seguidores;
+
+    public Viewer(long cpf_cnpj, String nome, String email, int num_seguidores, String nacionalidade, String status, String descricao ){
+        
+        this.cpf_cnpj = cpf_cnpj;
+        this.nome = nome;
+        this.email = email;
+        this.nacionalidade = nacionalidade;
+        this.num_seguidores = num_seguidores;
+        this.status = status;
+        this.descricao = descricao;
+    }
+
+    public long getCpf_cnpj() {
+        return cpf_cnpj;
+    }
+
+    public void setCpf_cnpj(long cpf_cnpj) {
+        this.cpf_cnpj = cpf_cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getNum_seguidores() {
+        return num_seguidores;
+    }
+
+    public void setNum_seguidores(int num_seguidores) {
+        this.num_seguidores = num_seguidores;
+    }
+
+    @Override
+    public String toString() {
+        return "Viewer = cpf_cnpj: " + cpf_cnpj + ", nome: " + nome + ", email: " + email + ", nacionalidade: "+ nacionalidade + ", status: " + status + ", descricao: " + descricao + ", num_seguidores: "+ num_seguidores + "\n";
+    }
+
+
+}
