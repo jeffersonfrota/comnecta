1d68f1a298b0abc5ceb081781f2931fb70ac991a
Jefferson Frota <jeffersonfrotacampos@gmail.com>
2023-11-27 21:47:00 -0300

    Add a classe streamer

---
+++ src/main/java/com/example/util/Streamer.java
@@ -0,0 +1,8 @@
+package com.example.util;
+
+public class Streamer extends Conta{
+    
+    public Streamer(long cpf_cnpj, String nome, String email, int num_seguidores, String nacionalidade, String status, String descricao){
+        super(cpf_cnpj, nome, email, num_seguidores, nacionalidade, status, descricao);
+    }
+}
